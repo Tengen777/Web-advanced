fcb44d53c1bf170e8ab4781bd3b566d3e8d064a9
Tengen777 <theo30m@gmail.com>
2024-06-03 22:08:07 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+alert("Hello World") // oefening 1
+
+// oefening 2
+let a = 10; 
+a = a+5;
+console.log(a);
+
+// oefening 3
+let leeftijd = prompt("wat is je leeftijd");
+
+    if (leeftijd > 18){
+        console.log("welkom");
+
+    } else {
+        console.log("No entry");
+    }
+    //
